fbbd1e7b512ca0cec79baff860b80231dd691b43
Polina Nechaeva <polina2000_21@mail.ru>
2023-03-17 23:50:57 +0300

    english version

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 
+admin.autodiscover()
+
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', include('mainapp.urls', namespace='mainapp')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
+
+urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
