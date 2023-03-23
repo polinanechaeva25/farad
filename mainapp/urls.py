c844d40293a1673a6c0622284e5fcac9cb12fc9c
juliojm1310 <juliojm1310@gmail.com>
2023-03-23 23:44:47 +0300

    Fix static volume

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from mainapp.views import MainListView
+    # , EmailView
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', MainListView.as_view(), name='index'),
+    # path('email/', EmailView.as_view(), name='send_email'),
 ]
